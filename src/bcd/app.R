f292e0f6225dcee90f620eb3e931dcb3f32b2e9a
apjansing <apjansing@gmail.com>
2018-06-20 23:10:04 -0400

    added ability to filter on incident_type_primary

--- src/bcd/app.R
+++ src/bcd/app.R
@@ -12,26 +12,64 @@ library( leaflet )
 buf_cameras = mongo(collection = "cameras", db = "buffalo") # create connection, database and collection
 buf_crimes = mongo(collection = "crimes", db = "buffalo") # create connection, database and collection
 
+crime_types <- c( 'UUV',
+                  'LARCENY/THEFT',
+                  'BURGLARY',
+                  'RAPE',
+                  'SEXUAL ABUSE',
+                  'ASSAULT',
+                  'ROBBERY',
+                  'THEFT OF SERVICES',
+                  'MURDER',
+                  'AGGR ASSAULT',
+                  'CRIM NEGLIGENT HOMICIDE',
+                  'AGG ASSAULT ON P/OFFICER',
+                  'Theft of Vehicle',
+                  'Sexual Assault',
+                  'Theft',
+                  'Breaking & Entering',
+                  'Assault',
+                  'MANSLAUGHTER',
+                  'Other Sexual Offense')
+
+# incident_id, case_number, incident_datetime, incident_type_primary, incident_description, clearance_type, address_1,
+# address_2, city, state, zip, country, latitude, longitude, created_at, updated_at, location, hour_of_day,
+# day_of_week, parent_incident_type, closestCamera
 plotCrimes <- function( crimeCollection, options ){
   crimes <- ''
+  
+  limiter = '{'
   if( options$dateStart != options$dateEnd ){
-    limiter <- paste0('{"incident_datetime":{"$lte" : "',  format(options$dateEnd, format="%m/%d/%Y"), '", "$gte" : "', format(options$dateStart, format="%m/%d/%Y"),'"} }')
-    print(limiter)
-    crimes <- crimeCollection$find(limiter, limit = options$limit)
-  }else {
-    crimes <- crimeCollection$find()
+    limiter = paste0(limiter, '"incident_datetime":{"$lte" : "',  format(options$dateEnd, format="%m/%d/%Y"), '", "$gte" : "', format(options$dateStart, format="%m/%d/%Y"),'"},')
   }
-    
   
-  # incident_id, case_number, incident_datetime, incident_type_primary, incident_description, clearance_type, address_1,
-  # address_2, city, state, zip, country, latitude, longitude, created_at, updated_at, location, hour_of_day,
-  # day_of_week, parent_incident_type, closestCamera
   
+  if( options$crimez != '' ){
+    limiter = paste0(limiter, '"incident_type_primary":{"$in" : [ ', options$crimez, ' ]},')
+  }
+  
+  
+
+  
+  
+  if(endsWith(limiter, ',')){
+    limiter <- substr(limiter, start = 1, stop = nchar(limiter)-1 )
+  }
+  limiter <- paste0(limiter, '}')
+  
+  print(limiter)
+  
+  if(limiter == '{}') {
+    crimes <- crimeCollection$find(limit = options$limit)
+  } else {
+    crimes <- crimeCollection$find(limiter, limit = options$limit)
+  }
+
   buf_map <- leaflet() %>%
     addTiles('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png') %>%  # Add default OpenStreetMap map tiles
     addCircles( lng=crimes$longitude, lat=crimes$latitude, radius=5, 
                 color="#ffa500", stroke = TRUE, fillOpacity = 0.5, popup=getDescription( crimes ))
-
+  
   return(buf_map)
 }
 
@@ -48,62 +86,41 @@ getDescription <- function( crime ){
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Buffalo Crime Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-     sidebarPanel(
-       numericInput("limit", "Max number of results", 500, min = 1),
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30
-                  ),
-      selectInput( "Crimes", "Choose a crimes:",
-        choices = c( 'UUV',
-                     'LARCENY/THEFT',
-                     'BURGLARY',
-                     'RAPE',
-                     'SEXUAL ABUSE',
-                     'ASSAULT',
-                     'ROBBERY',
-                     'THEFT OF SERVICES',
-                     'MURDER',
-                     'AGGR ASSAULT',
-                     'CRIM NEGLIGENT HOMICIDE',
-                     'AGG ASSAULT ON P/OFFICER',
-                     'Theft of Vehicle',
-                     'Sexual Assault',
-                     'Theft',
-                     'Breaking & Entering',
-                     'Assault',
-                     'MANSLAUGHTER',
-                     'Other Sexual Offense'
-                    ), multiple = TRUE
-                  ),
-      dateRangeInput("dateRange", "Date Range", format = 'mm/dd/yyyy', start = '01/01/2000')
+  
+  # Application title
+  titlePanel("Buffalo Crime Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("limit", "Max number of results", 500, min = 1),
+      selectInput( "crimes", "Choose a crimes:",
+                   choices = crime_types, selected = crime_types, multiple = TRUE
       ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-        leafletOutput("distPlot"), height = 500, width = 600
-      )
+      dateRangeInput("dateRange", "Date Range", format = 'mm/dd/yyyy', start = '01/01/2000')
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      leafletOutput("distPlot"), height = 500, width = 600
     )
   )
+)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
   
   
-   output$distPlot <- renderLeaflet({
-     map
-     d <- data.frame( dateStart = input$dateRange[1], dateEnd = input$dateRange[2], limit = input$limit )
-     plotCrimes(crimeCollection = buf_crimes, options = d)
-   })
+  
+  output$distPlot <- renderLeaflet({
+    map
+    
+    # crimez <- paste(shQuote(input$crimes), collapse=", ")
+    crimez <- paste0(sprintf("\"%s\"", input$crimes), collapse = ", ")
+
+    d <- data.frame( dateStart = input$dateRange[1], dateEnd = input$dateRange[2], limit = input$limit, crimez = crimez )
+    plotCrimes(crimeCollection = buf_crimes, options = d)
+  })
 }
 
 # Run the application 
